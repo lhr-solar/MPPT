f377c25d363d61c1f479f9288d511c90752109f2
Afnan Mir <55025482+afnanmmir@users.noreply.github.com>
2021-07-04 23:15:08 -0500

    Update LocalMPPTAlgorithm.cpp

--- mppt/LocalMPPTAlgos/LocalMPPTAlgorithm.cpp
+++ mppt/LocalMPPTAlgos/LocalMPPTAlgorithm.cpp
@@ -11,7 +11,6 @@
  * is the parent class of the Local MPPT algroithms.
  */
 #define MAX_VOLTAGE 0.8
-#pragma once
 #include "LocalMPPTAlgorithm.h"
 #include <chrono>
 
@@ -25,14 +24,22 @@ LocalMPPTAlgorithm::LocalMPPTAlgorithm(const PinName pin, StrideType type) : tra
     rightBound = MAX_VOLTAGE;
     arrVoltOld = 0.0;
     arrPowOld = 0.0;
-    if(type == FIXED){
-        stride = new Stride()
-    }else if(type == ADAPTIVE){
-        stride = new AdaptiveStride()
-    }else if(type == OPTIMAL){
-        stride = new OptimalStride()
-    }else if(type == BISECTION){
-        stride = new BisectionStride();
+    switch(type){
+        case FIXED:
+            stride = new Stride();
+            break;
+        case ADAPTIVE:
+            stride = new AdaptiveStride();
+            break;
+        case OPTIMAL:
+            stride = new OptimalStride();
+            break;
+        case BISECTION:
+            stride = new BisectionStride();
+            break;
+        default:
+            stride = new Stride();
+            break;
     }
 }
 
@@ -53,7 +60,7 @@ const char* LocalMPPTAlgorithm::get_stride_type() const{
     return stride.get_name();
 }
 
-void LocalMPPTAlgorithm::getReferenceVoltage
+double LocalMPPTAlgorithm::getReferenceVoltage
 (double arrayVoltage, double arrayCurrent){
     return 0.0;
 }
